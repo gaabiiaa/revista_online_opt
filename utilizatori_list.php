0bbec2589c3937ac25c027f696ad20a2ea17f6af
gaabiiaa <gabiaygan@yahoo.com>
2025-11-21 22:51:12 +0200

    Update utilizatori_list.php

--- utilizatori_list.php
+++ utilizatori_list.php
@@ -2,9 +2,6 @@
 session_start();
 require 'db.php';
 
-// optional: verifici daca e admin
-// if ($_SESSION['user_role'] !== 'admin') { die("Acces interzis."); }
-
 $stmt = $conn->query("SELECT * FROM utilizatori ORDER BY id_utilizator ASC");
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -27,3 +24,4 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </tr>
     <?php endforeach; ?>
 </table>
+
